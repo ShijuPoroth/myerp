d3569b252da5cd7cb2284980217a1cdf65819f1f
ShijuPoroth <p.shiju@outlook.com>
2026-09-12 14:45:30 +0300

    Add second CSP hash for Cloudflare injected script

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(helmet({
       defaultSrc: ["'self'"],
       styleSrc: ["'self'", "https://cdnjs.cloudflare.com"],
       styleSrcAttr: ["'unsafe-inline'"],
-      scriptSrc: ["'self'", "https://cdnjs.cloudflare.com", "'sha256-nLWmrzOF3jLZ+2SX/ZZNUsLMkyR2x9HzZPo+Ow80LcE='"],
+      scriptSrc: ["'self'", "https://cdnjs.cloudflare.com", "'sha256-nLWmrzOF3jLZ+2SX/ZZNUsLMkyR2x9HzZPo+Ow80LcE='", "'sha256-s0uowI2IAIZXG34CYq41+5xje3Yz21P2TLtimlAn3Lc='"],
       scriptSrcAttr: ["'unsafe-inline'"],
       objectSrc: ["'none'"],
       baseUri: ["'none'"],
