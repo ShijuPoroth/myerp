c65cf5fc5a4cd2724062474b63487de25087a973
ShijuPoroth <p.shiju@outlook.com>
2026-09-12 15:19:45 +0300

    Add second Cloudflare challenge platform CSP hash

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(helmet({
       defaultSrc: ["'self'"],
       styleSrc: ["'self'", "https://cdnjs.cloudflare.com"],
       styleSrcAttr: ["'unsafe-inline'"],
-      scriptSrc: ["'self'", "https://cdnjs.cloudflare.com", "'sha256-7z++peN0VDVzFLVcOcv50T9xKH2dqyjrJszECytR1UM='"],
+      scriptSrc: ["'self'", "https://cdnjs.cloudflare.com", "'sha256-7z++peN0VDVzFLVcOcv50T9xKH2dqyjrJszECytR1UM='", "'sha256-/VXBoR64QXu5fLGu7O29afXe9yQUUUa5naTHMDpNfqs='"],
       scriptSrcAttr: ["'unsafe-inline'"],
       objectSrc: ["'none'"],
       baseUri: ["'none'"],
